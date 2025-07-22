fe5eb0b21986e7743ad0e0001501abf6aee6f265
Abhishek raj <abhi.yashsinha2003@gmail.com>
2025-07-22 14:41:06 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+
+music = {
+    "closer": "https://www.youtube.com/watch?v=PT2_F-1esPk",
+    "shape of you": "https://www.youtube.com/watch?v=JGwWNGJdvx8",
+    "believer": "https://www.youtube.com/watch?v=7wtfhZwyrcc"
+}
+
+
